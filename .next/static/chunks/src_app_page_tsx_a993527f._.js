a27e6c77fa7f8d06c544308439f0bb7b59c1fb3a
Arjay <164826321+EisenDev@users.noreply.github.com>
2025-06-21 23:19:09 +0800

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1750519148973

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_67d2f896._.js",
+  "static/chunks/src_74c6a133._.js"
+],
+    source: "dynamic"
+});
